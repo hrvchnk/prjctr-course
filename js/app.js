8ca0eeec69e2eb8296f4de235d7a190013b2ce36
Vlad <137286644+hrvchnk@users.noreply.github.com>
2025-01-15 18:59:21 +0200

    Revert "Змінив стилі для комопнентів; прибрав form; додав збереження в локаль…"
    This reverts commit 4518912bd5154d4eb566d38c648df5e55f432bdf.

--- js/app.js
+++ js/app.js
@@ -13,3 +13,45 @@ document.querySelectorAll('.tabs__toggle').forEach(toggle => {
 		document.getElementById(tabId).classList.add('is-active');
 	});
 });
+
+// Task 1: Calculate the range between two dates
+function calculateDateRange(startDate, endDate, rangeType) {
+	// const startDate = new Date(document.getElementById('start-date').value);
+	// const endDate = new Date(document.getElementById('end-date').value);
+	// const rangeType = document.getElementById('range-selector').value;
+
+	if (startDate > endDate) {
+		alert('Error startDate > endDate');
+		return;
+	}
+
+	const diffInMs = Math.abs(endDate - startDate);
+
+	let result;
+
+	switch (rangeType) {
+		case 'seconds':
+			return `${diffInMs / 1000} seconds`;
+		case 'minutes':
+			return `${diffInMs / (1000 * 60)} minutes`;
+		case 'hours':
+			return `${diffInMs / (1000 * 60 * 60)} hours`;
+		case 'days':
+			return `${diffInMs / (1000 * 60 * 60 * 24)} days`;
+		default:
+			return 'Error';
+	}
+}
+
+// btn
+document.getElementById('day-button').addEventListener('click', function () {
+	const form = document.getElementById('form');
+
+	const formData = new FormData(form);
+
+	const data = Object.fromEntries(formData.entries());
+
+	console.log('Form data:', data);
+});
+
+//
